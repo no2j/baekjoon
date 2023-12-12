8dfce02402f51d857ca102016f6c3e5e4d2a5b2c
no2j <jungwoo343@naver.com>
2023-12-12 16:33:46 +0900

    브루트포스 19532

---
+++ src/com/jw/beakjoon/lv12/Bj19532.java
@@ -0,0 +1,43 @@
+package com.jw.beakjoon.lv12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj19532 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+		
+		boolean flag = false;
+		
+		for(int i=-999; i<1000; i++) {
+			for(int j=-999; j<1000; j++) {
+				if((a*i+b*j==c) && (d*i+e*j==f)) {
+					System.out.println(i + " " + j);
+					flag = true;
+					break;
+				}
+			}
+			if(flag == true) {
+				break;
+			}
+		}
+		
+		br.close();
+		
+	}
+	
+}
